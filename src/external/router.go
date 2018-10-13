c7d181473c0c01adfd93eb7db4c5544b5aaf3af0
RikiyaFujii <ridai22hanriku@gmail.com>
2018-10-13 13:49:29 +0900

    rest

--- src/external/router.go
+++ src/external/router.go
@@ -9,7 +9,7 @@ import (
 // Router ...
 func Router(r *mux.Router) {
 	usersController := adapter.NewUserController(*database.NewSQLHandler())
-	r.HandleFunc("/create", usersController.Create).Methods("POST")
+	r.HandleFunc("/users", usersController.Create).Methods("POST")
 	r.HandleFunc("/users", usersController.FindAll).Methods("GET")
 	r.HandleFunc("/users/{id}", usersController.Update).Methods("PUT")
 	r.HandleFunc("/users/{id}", usersController.Delete).Methods("DELETE")
